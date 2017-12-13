292faf972e349461a836a05f880d2f23b0fbad36
barni211 <barni211@gmail.com>
2017-12-14 00:27:52 +0100

    first commit

---
+++ ProgramowanieUslugSieciowych-Projekt/ProgramowanieUslugSieciowych-Projekt/Program.cs
@@ -0,0 +1,100 @@
+﻿using Logger;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Net.Sockets;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ProgramowanieUslugSieciowych_Projekt
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        /// 
+        static TcpListener tcpListener = new TcpListener(1800);
+        //static TcpListener listenerForLogger = new TcpListener(1800);
+        static int clientCounter = 0;
+        static Logger.Logger log;
+        static Server s;
+
+        static void Main()
+        {
+            FileOutputter file = new FileOutputter();
+            log = new Logger.Logger();
+            log.SetLoggerLevel(Level.DEBUG);
+            log.SetLoggerOutput(file);
+
+
+            tcpListener.Start();
+            //listenerForLogger.Start();
+            Thread threadForServer = new Thread(RunServer);
+            threadForServer.Start();
+
+            s = new Server();
+            s.ShowDialog();
+
+        }
+
+  
+        public static void RunServer()
+        {
+            for (;;)
+            {
+                Socket socketForClient = tcpListener.AcceptSocket();
+                Thread thread = new Thread(() => ConnectedClient(socketForClient));
+                thread.Start();
+            }
+        }
+
+     
+
+        static void ConnectedClient(Socket socketForClient)
+        {
+            //Debugger.Break();
+            //Console.ReadKey();
+           // Debug.Print("Client is connected, hello");
+            if (socketForClient.Connected)
+            {
+                s.increment();
+                log.WriteLog(Level.DEBUG, "Client:" + socketForClient.RemoteEndPoint + " now connected to server.");
+                NetworkStream networkStream = new NetworkStream(socketForClient);
+                System.IO.StreamWriter streamWriter =
+                new System.IO.StreamWriter(networkStream);
+                System.IO.StreamReader streamReader =
+                new System.IO.StreamReader(networkStream);
+
+                //here we recieve client's text if any.
+                while (true)
+                {
+
+                    string theString = streamReader.ReadLine();
+                    Debug.Print(theString);
+                    //  if (theString.Length > 10)
+                    //  {
+                    //      //log.WriteLog(Level.ALERT, "Message recieved by client: " + socketForClient.RemoteEndPoint + ": " + theString);
+                    //  }
+                    //  else
+                    //  {
+                    //      //log.WriteLog(Level.DEBUG, "Message recieved by client: " + socketForClient.RemoteEndPoint + ": " + theString);
+                    //  }
+                    if (theString == "exit")
+                        break;
+                }
+                streamReader.Close();
+                networkStream.Close();
+                streamWriter.Close();
+                s.decrement();
+            }
+          
+            socketForClient.Close();
+
+            //log.WriteLog(Level.INFO, "Press any key to exit from server program");
+            //Console.ReadKey();
+        }
+    }
+}
---
+++ ProgramowanieUslugSieciowych-Projekt/ProgramowanieUslugSieciowych-Projekt/Server.cs
@@ -0,0 +1,47 @@
+﻿using Logger;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ProgramowanieUslugSieciowych_Projekt
+{
+    public partial class Server : Form
+    {
+        static int clientCounter = 0;
+        public Server()
+        {
+            InitializeComponent();
+        }
+
+        private void SetCounterView()
+        {
+            threadCounter.Text = clientCounter.ToString();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            SetCounterView();
+        }
+
+        public void increment()
+        {
+            clientCounter++;
+        }
+
+        public void decrement()
+        {
+            clientCounter--;
+        }
+    }
+
+
+}
